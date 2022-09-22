4ea488e62c0dec51dc60363cd26689a407e5794a
Leo Angulo <leo490@revature.net>
2022-09-21 20:58:50 -0700

    public void loginFunction(){
        }

--- Project1/src/Main.java
+++ Project1/src/Main.java
@@ -1,6 +1,37 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("---------------------------------------------");
+        System.out.println("Welcome to the Murtech Reimbursement System");
+        System.out.println("---------------------------------------------");
+
+        System.out.println("1. Login");
+        System.out.println("2. Register");
+        System.out.println("3. Exit");
+
+        String userInput;
+        do {
+            Scanner scnrObj = new Scanner(System.in);
+            userInput = scnrObj.nextLine();
+
+            if (userInput.equals("1")) {
+                loginFunction();
+            } else if (userInput.equals("2")) {
+                registerFunction();
+            } else if (userInput.equals("3")) {
+                System.out.println("Thank you, have a nice day!");
+            } else {
+                System.out.println("Please enter a valid option.");
+            }
+        } while (!userInput.equals("3"));
     }
+    public static void loginFunction(){
+
+    }
+
+    public static void registerFunction(){
+
+    }
+
 }
